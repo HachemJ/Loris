25655d72bdb8884c3f5890025007f8cb6cbf0c86
HachemJ <jad.elhachem.pro@gmail.com>
2026-09-08 14:31:41 -0400

    add missing translation

--- modules/login/jsx/loginIndex.js
+++ modules/login/jsx/loginIndex.js
@@ -97,8 +97,9 @@ class Login extends Component {
         .then((resp) => {
           if (!resp.ok) {
             this.setState({
-              resetTokenError: 'Your password reset token is invalid or has'
-              + ' expired. Please request a new password reset link.',
+              resetTokenError: t('Your password reset token is invalid or has'
+                + ' expired. Please request a new password reset link.',
+                {ns: 'login'}),
             });
             return null;
           }
