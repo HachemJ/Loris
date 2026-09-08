395f10786e8b29bdd4688ffa163ac5969d772012
HachemJ <jad.elhachem.pro@gmail.com>
2026-09-08 15:12:44 -0400

    fix lint

--- modules/login/jsx/loginIndex.js
+++ modules/login/jsx/loginIndex.js
@@ -97,9 +97,10 @@ class Login extends Component {
         .then((resp) => {
           if (!resp.ok) {
             this.setState({
-              resetTokenError: this.props.t('Your password reset token is invalid'
-                + ' or has expired. Please request a new password reset link.',
-                {ns: 'login'}),
+              resetTokenError: this.props.t('Your password reset token is'
+              + ' invalid or has expired. Please request a new password reset'
+              + ' link.',
+              {ns: 'login'}),
             });
             return null;
           }
